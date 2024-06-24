7a51ef691cecf40ad0953b7c8fc6ab64e13767fe
radovansekulic <radovan.sekuliic@gmail.com>
2024-06-24 10:31:52 -0700

    Created promotion bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
+import PromotionBar from './components/PromotionBar'
+
 export default function App() {
   return (
-    <h1 className="container mx-auto mt-20 px-4 text-3xl font-bold underline">
-      #KORISNAPRODAVNICA
-    </h1>
+    <PromotionBar />
   )
 }
---
+++ frontend/src/components/PromotionBar.jsx
@@ -0,0 +1,7 @@
+export default function PromotionBar() {
+    return (
+        <div className="bg-[#E1FF3E] text-[#3B3933] p-2 text-center">
+            <p className="font-[Inter]">Besplatna dostava za iznose veće od 2000RSD</p>
+        </div>
+    )
+}
