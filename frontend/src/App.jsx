bf55e55dc23472872269d387df5b1b466b829f5a
radovansekulic <radovan.sekuliic@gmail.com>
2024-06-25 09:57:59 -0700

    Created scrollbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import PromotionBar from './components/PromotionBar'
 import Header from './components/Header'
+import ScrollBar from './components/ScrollBar'
 
 export default function App() {
   return (
     <>
       <PromotionBar />
       <Header />
+      <ScrollBar />
     </>
   )
 }
