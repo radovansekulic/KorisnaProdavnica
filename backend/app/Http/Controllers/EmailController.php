b0ef5487ee78450036ca5119afaa849a8c0aa903
radovansekulic <radovan.sekuliic@gmail.com>
2024-06-28 05:49:18 -0700

    Created email validator

--- backend/app/Http/Controllers/EmailController.php
+++ backend/app/Http/Controllers/EmailController.php
@@ -2,17 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CheckEmailRequest;
+use App\Models\Email; // Pretpostavljamo da koristiš Email model
 use Illuminate\Http\Request;
-use App\Models\Email;
 
 class EmailController extends Controller
 {
-    public function index(Request $req)
+    public function index(CheckEmailRequest $request)
     {
-        Email::create([
-            'email' => $req['email'],
-        ]);
+        $emailExists = Email::where('email', $request->email)->exists();
 
-        return ["message" => "Uspešno Ste Registrovani"];
+        $emailExists = Email::where('email', $request->email)->exists();
+
+        return $emailExists 
+            ? ['message' => 'Email već postoji u bazi.'] : (Email::create(['email' => $request->email]) 
+            ? ['message' => 'Uspešno Ste Registrovani'] : ['message' => 'Došlo je do greške prilikom registracije']);
+
     }
 }
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -43,24 +43,24 @@ export default function Footer() {
                     <a href="/collection">Instagram</a>
                 </u>
             </div>
-            {message && (
-                <>
-                    <div className="container mx-auto rounded-md p-5 my-5 flex items-center justify-between border borer-[#f0fdf4] bg-[#f0fdf4]">
-                        <div className="flex gap-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 20 20" fill="#4ade80" aria-hidden="true"
-                                className="nz sb axv">
-                                <path fill-rule="evenodd"
-                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                            <span>{message}</span>
-                        </div>
-                    </div>
-                </>
-            )}
             <div className="mb-5 col-span-3">
                 <h1 className="mb-2 font-medium">NEWSLETTER</h1>
                 <p className="mb-4">Budite prvi koji će saznati o najnovijim trendovima i ekskluzivnim ponudama</p>
+                {message && (
+                    <>
+                        <div className="rounded-md p-5 my-5 flex items-center justify-between border borer-[#f0fdf4] bg-[#f0fdf4]">
+                            <div className="flex gap-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 20 20" fill="#4ade80" aria-hidden="true"
+                                    className="nz sb axv">
+                                    <path fill-rule="evenodd"
+                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
+                                <span>{message}</span>
+                            </div>
+                        </div>
+                    </>
+                )}
                 <div className="flex items-center left-[-10px]">
                     <input type="mail" ref={email} name="email" className="rounded-md bg-white border focus:outline-none w-full h-[54px] px-4 py-2" placeholder="Email Adresa" />
                     <button onClick={handleClick} className="rounded-lg py-2 px-5 h-[54px] text-2xl bg-rose-500 ms-[-10px] text-white">→</button>
